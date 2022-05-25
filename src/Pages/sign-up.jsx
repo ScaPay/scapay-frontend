59b9b31249cf3385e075f66e069d6498e7cddd8a
Steven <tobi4steve@gmail.com>
2022-05-25 13:56:18 +0100

    sign-up

--- src/Pages/sign-up.jsx
+++ src/Pages/sign-up.jsx
@@ -4,25 +4,25 @@ import DropDown from "../components/dropdown";
 const SignUp = () => {
 
     const options = [
-        { label: 'Fruit', value: 'fruit' },
-        { label: 'Vegetable', value: 'vegetable' },
-        { label: 'Meat', value: 'meat' },
+        { label: 'Nigeria', value: 'ng' },
+        { label: 'United States', value: 'us' },
+        { label: 'Algeria', value: 'dz' },
       ];
     
-      const [value, setValue] = useState('fruit');
+      const [value, setValue] = useState('us');
     
       const handleChange = (event) => {
         setValue(event.target.value);
       };
     
   return (
-    <div className="rounded-xl p-6 bg-white shadow-md text-sm max-w-screen-md mx-auto md:flex">
+    <div className="justify-center rounded-xl bg-white shadow-md text-sm max-w-screen-md mx-auto md:flex">
         <div className=""> 
             <form>
 
             <div className="text-slate-700">
             <span className="p-2 after:content-['*'] after:ml-0.5 after:text-red-500 block">Full name</span>
-            <input type="text" id="fullname" className="rounded-xl w-100 h-10 placeholde-text-slate-400 pl-[14px] border-zinc-600 border-2" placeholder="Firstname Lastname" />
+            <input type="text" id="fullname" className="rounded-xl h-10 placeholde-text-slate-400 pl-[14px] border-zinc-600 border-2" placeholder="Firstname Lastname" />
             </div>
             
             <div className="text-slate-700">
@@ -34,6 +34,17 @@ const SignUp = () => {
             <span className="p-2 after:content-['*'] after:ml-0.5 after:text-red-500 block text-slate-700">Country/Region</span>
             <DropDown options={options} value={value} onChange={handleChange} />
             </div>
+
+            <div className="text-slate-700">
+            <span className="p-2 after:content-['*'] after:ml-0.5 after:text-red-500 block text-slate-700">Password</span>
+            <input type="password" id="password" className="rounded-xl w-100 h-10 placeholder-text-slate-400 pl-[14px] border-zinc-600 border-2" placeholder="Password" />
+            </div>
+
+            <div className="text-slate-700">
+            <span className="p-2 after:content-['*'] after:ml-0.5 after:text-red-500 block text-slate-700">Phone number</span>
+            <input type="phone" id="phone" className="rounded-xl w-100 h-10 placeholder-text-slate-400 pl-[14px] border-zinc-600 border-2" placeholder="+" />
+            </div>
+
         </form>
         </div>
     </div>
